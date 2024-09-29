14ebd50051a17f9b5338d79b0a1c6923c1e308e6
Kirill Simakov <kirill.simakov@mpd.ae>
2024-09-30 02:46:24 +0300

    Small fixes for CI

--- src/s3_utils.py
+++ src/s3_utils.py
@@ -1,14 +1,8 @@
-from tenacity import retry, stop_after_attempt, wait_fixed, retry_if_exception_type
 import boto3
 from .config import CHUNK_SIZE
 from .aws_utils import s3_client
 from .logger import logger
 
-@retry(
-    stop=stop_after_attempt(5),
-    wait=wait_fixed(2),
-    retry=retry_if_exception_type((boto3.exceptions.Boto3Error, Exception))
-)
 def stream_s3_file(bucket, key, chunk_size=CHUNK_SIZE):
     """Stream lines from S3 file in a memory-efficient way using yield with retry."""
     try:
